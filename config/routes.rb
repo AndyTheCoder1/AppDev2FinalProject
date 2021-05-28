843d5323835701fc73737015f6ab94b4c498ce01
AndyTheCoder1 <abieber@chicagobooth.edu>
2021-05-28 21:20:51 +0000

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
+  devise_for :users
   resources :prompts
   resources :prompts_answers
   resources :date_requests
   resources :photos
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
