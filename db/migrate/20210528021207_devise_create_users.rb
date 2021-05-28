0c6c7ff71136741d86bd00127d03e2543c2ea6fa
AndyTheCoder1 <abieber@chicagobooth.edu>
2021-05-28 02:54:49 +0000

    created users and migrated

--- db/migrate/20210528021207_devise_create_users.rb
+++ db/migrate/20210528021207_devise_create_users.rb
@@ -2,6 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
+    enable_extension("citext")
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -32,29 +33,32 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
-      t.string :religion
-      t.string :political_affiliation
-      t.string :height
-      t.string :hometown
-      t.integer :age
-      t.string :job_title
-      t.string :school
-      t.string :family_plans
-      t.string :gender
-      t.string :drinking
-      t.string :smoking
-      t.string :undergrad_school
-      t.integer :daily_photo_reveals
-      t.integer :sent_follow_requests_count
-      t.integer :received_follow_requests_count
-      t.integer :own_photos_count
+      t.citext :username
+      t.string :religion, default: nil
+      t.string :political_affiliation, default: nil
+      t.string :height, default: nil
+      t.string :hometown, default: nil
+      t.integer :age, default: 0
+      t.string :job_title, default: nil
+      t.string :school, default: nil
+      t.string :family_plans, default: nil
+      t.string :gender, default: nil
+      t.string :drinking, default: nil
+      t.string :smoking, default: nil
+      t.string :undergrad_school, default: nil
+      t.string :romantic_interests, default: nil
+      t.integer :daily_photo_reveals, default: 0
+      t.integer :sent_follow_requests_count, default: 0
+      t.integer :received_follow_requests_count, default: 0
+      t.integer :own_photos_count, default: 0
+      t.boolean :active, default: true
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username,             unique: false
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_28_021207) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.citext "username"
+    t.string "religion"
+    t.string "political_affiliation"
+    t.string "height"
+    t.string "hometown"
+    t.integer "age", default: 0
+    t.string "job_title"
+    t.string "school"
+    t.string "family_plans"
+    t.string "gender"
+    t.string "drinking"
+    t.string "smoking"
+    t.string "undergrad_school"
+    t.string "romantic_interests"
+    t.integer "daily_photo_reveals", default: 0
+    t.integer "sent_follow_requests_count", default: 0
+    t.integer "received_follow_requests_count", default: 0
+    t.integer "own_photos_count", default: 0
+    t.boolean "active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username"
+  end
 
 end
